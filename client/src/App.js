f02dfd9da80748055f8b7237411d677c34504cb6
Ryan Hauk <rhauk@fossil.com>
2019-11-19 16:13:12 -0600

    Changing the render a bit on App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Fib from './Fib';
 import OtherPage from './OtherPage';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <header>
-          <h1 className="App-title">Fib Calculator</h1>
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-        </header>
-        <div>
-          <Route exact path="/" component={Fib} />
-          <Route path="/otherpage" component={OtherPage} />
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <header>
+            <h1 className="App-title">Fib Calculator v2</h1>
+            <Link to="/">Home</Link>
+            <Link to="/otherpage">Other Page</Link>
+          </header>
+          <div>
+            <Route exact path="/" component={Fib} />
+            <Route path="/otherpage" component={OtherPage} />
+          </div>
         </div>
-      </div>
-    </Router>
-  );
+      </Router>
+    );
+  }
 }
 
 export default App;
